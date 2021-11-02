b484a58704327ea3abf9f032c172de038729913d
yunsuhvvan <87231093+yunsuhvvan@users.noreply.github.com>
2021-11-02 17:46:49 +0900

    Add files via upload

--- jspstudy/workspace/02_JSP/src/ex09_session/Member.java
+++ jspstudy/workspace/02_JSP/src/ex09_session/Member.java
@@ -1,38 +1,48 @@
 package ex09_session;
 
 public class Member {
-
+	
 	private String userId;
 	private String userPw;
 	private String userName;
 	
 	public Member() {
 		
+		
 	}
+
 	public Member(String userId, String userPw, String userName) {
 		super();
 		this.userId = userId;
 		this.userPw = userPw;
 		this.userName = userName;
 	}
-	
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public String getUserPw() {
 		return userPw;
 	}
+
 	public void setUserPw(String userPw) {
 		this.userPw = userPw;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
+	
+
 	
 }
