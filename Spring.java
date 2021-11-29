3020d8923f8f6095e4f2a3b90e066f5644d5bf3b
yunsuhvvan <87231093+yunsuhvvan@users.noreply.github.com>
2021-11-29 14:34:59 +0900

    Update and rename Spring.txt to Spring.java

---
+++ Spring.java
@@ -0,0 +1,18 @@
+Spring : 개발자에 봄을 선사한다.
+
+1. Spring legacy Project : sts version -> 3.x 
+    - Spring MVC Project
+    - Mybatis
+2. Spring boot : sts version -> 4.x
+    - Mybatis or JPA
+    - jsp or html + template(thymeleaf)
+    - war or jar
+    
+공식홈페이지 : spring.io
+sts 3.x
+sts 4.x
+
+진행 :  sts 4.x 다운로드 + sts 3.x 기능 추가
+https://spring.io/tools -> 4.12.1 window 다운
+https://github.com/spring-projects/toolsuite-distribution/wiki/Spring-Tool-Suite-3 -> Latest STS3 Downloads 
+    
